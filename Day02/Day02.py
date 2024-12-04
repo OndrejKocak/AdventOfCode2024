9abf75b9e217bd4da2cd5dd486df28a3df2989ea
OndrejKocak <ondrej.kocak@gmail.com>
2024-12-05 00:12:04 +0100

    Day02 | part2

--- Day02/Day02.py
+++ Day02/Day02.py
@@ -19,4 +19,37 @@ def reportSafetyCheck(path):
             total += 1
     return total
 
+
+def tolerateASingleBadLevel(path):
+    f = open(path, "r")
+    total = 0
+    for line in f.readlines():
+        numbers = line.split()
+        if checkNumbers(numbers, False):
+            total += 1
+    return total
+
+
+def checkNumbers(numbers, tolerated):
+    increase = True
+    n0 = int(numbers[0])
+    n1 = int(numbers[1])
+    if n0 > n1:
+        increase = False
+    for i in range(len(numbers)-1):
+        n0 = int(numbers[i])
+        n1 = int(numbers[i+1])
+        if (increase and n0 > n1) or (not increase and n0 < n1) or (abs(n0-n1) > 3) or (n0-n1 == 0):
+            if not tolerated:
+                for j in range(len(numbers)):
+                    numbersPoped = numbers.copy()
+                    numbersPoped.pop(j)
+                    if checkNumbers(numbersPoped, True):
+                        return True
+                return False
+            return False
+    return True
+    
+
 print(reportSafetyCheck("input02.txt"))
+print(tolerateASingleBadLevel("input02.txt"))
